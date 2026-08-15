f3e487b2a8377400c6459631033bb020f1734fcf
R4mboX <R4mboX@users.noreply.github.com>
2026-08-15 21:08:30 +0200

    Externe Library-Abhaengigkeit paketfest aufloesen

--- build.zig
+++ build.zig
@@ -8,6 +8,10 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const sdk_build = b.lazyImport(@This(), "r4os_sdk") orelse return;
     const sdk_dep = b.dependencyFromBuildZig(sdk_build, .{});
+    const libraries_build = b.lazyImport(@This(), "r4os_libraries") orelse return;
+    const libraries_dep = b.dependencyFromBuildZig(libraries_build, .{});
     const sdk = sdk_build.sdk(b, sdk_dep, .{});
-    _ = sdk.addR4MF(b.path("module.R4MF"));
+    _ = sdk.addR4MFWithOptions(b.path("module.R4MF"), .{
+        .zig_module_roots = &.{libraries_dep.namedLazyPath("r4std_zig_binding")},
+    });
 }
